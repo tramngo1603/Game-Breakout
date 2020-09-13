aea2a7f70bc3366499fe0cba58c1b3382a386ce0
Tram Ngo <tramngo1603@gmail.com>
2020-09-13 15:25:25 -0700

    First commits

---
+++ index.js
@@ -0,0 +1,71 @@
+const rulesBtn = document.getElementById("rules-btn")
+const closeBtn = document.getElementById("close-btn")
+const rules = document.getElementById("rules")
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+let score = 0
+
+// create ball props
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4
+}
+
+// create paddle props
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 40,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0
+}
+
+// draw ball on canvas
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#6fd6ff";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// draw paddle on canvas
+function drawPaddle() {
+  ctx.beginPath()
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = "#6fd6ff";
+  ctx.fill();
+  ctx.closePath()
+}
+
+function draw() {
+  drawBall()
+  drawPaddle()
+  drawScore()
+}
+
+
+function drawScore() {
+  ctx.font = "20px Arial";
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
+}
+
+draw()
+
+
+// show and close rules
+rulesBtn.addEventListener("click", () => {
+  rules.classList.add("show");
+})
+
+closeBtn.addEventListener("click", () => {
+  rules.classList.remove("show");
+})
+
+// 1. create the canvas context
